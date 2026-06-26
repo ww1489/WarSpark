1eb6e1343910eabb1e495eed24b30eec49f7983f
ww1489 <15722871083@163.com>
2026-06-26 14:29:41 +0800

    Add RankingService with 6 ranking endpoints and caching

---
+++ internal/service/cocapi_fake_test.go
@@ -0,0 +1,80 @@
+package service
+
+import (
+	"context"
+
+	cocapi "github.com/ww1489/WarSpark/pkg/cocapi"
+)
+
+type fakeCocapiClient struct {
+	locations       cocapi.LocationListResponse
+	clanRanking     cocapi.ClanRankingListResponse
+	playerRanking   cocapi.PlayerRankingListResponse
+	capitalRanking  cocapi.ClanCapitalRankingListResponse
+	builderClan     cocapi.ClanBuilderBaseRankingListResponse
+	builderPlayer   cocapi.PlayerBuilderBaseRankingListResponse
+	leagues         cocapi.LeagueListResponse
+	league          cocapi.League
+	leagueSeasons   cocapi.LeagueSeasonListResponse
+	leagueRankings  cocapi.PlayerRankingListResponse
+	leagueTiers     cocapi.LeagueTierListResponse
+	leagueTier      cocapi.LeagueTier
+	leagueHistory   cocapi.LeagueSeasonResultListResponse
+	warLeagues      cocapi.WarLeagueListResponse
+	warLeague       cocapi.WarLeague
+	clanLabels      cocapi.LabelListResponse
+	playerLabels    cocapi.LabelListResponse
+	err             error
+}
+
+func (f *fakeCocapiClient) GetLocations(ctx context.Context, query cocapi.QueryGetLocations) (cocapi.LocationListResponse, error) {
+	return f.locations, f.err
+}
+func (f *fakeCocapiClient) GetClanRanking(ctx context.Context, locationID string, query cocapi.QueryGetClanRanking) (cocapi.ClanRankingListResponse, error) {
+	return f.clanRanking, f.err
+}
+func (f *fakeCocapiClient) GetPlayerRanking(ctx context.Context, locationID string, query cocapi.QueryGetPlayerRanking) (cocapi.PlayerRankingListResponse, error) {
+	return f.playerRanking, f.err
+}
+func (f *fakeCocapiClient) GetClanCapitalRanking(ctx context.Context, locationID string, query cocapi.QueryGetClanCapitalRanking) (cocapi.ClanCapitalRankingListResponse, error) {
+	return f.capitalRanking, f.err
+}
+func (f *fakeCocapiClient) GetClanBuilderBaseRanking(ctx context.Context, locationID string, query cocapi.QueryGetClanBuilderBaseRanking) (cocapi.ClanBuilderBaseRankingListResponse, error) {
+	return f.builderClan, f.err
+}
+func (f *fakeCocapiClient) GetPlayerBuilderBaseRanking(ctx context.Context, locationID string, query cocapi.QueryGetPlayerBuilderBaseRanking) (cocapi.PlayerBuilderBaseRankingListResponse, error) {
+	return f.builderPlayer, f.err
+}
+func (f *fakeCocapiClient) GetLeagues(ctx context.Context, query cocapi.QueryGetLeagues) (cocapi.LeagueListResponse, error) {
+	return f.leagues, f.err
+}
+func (f *fakeCocapiClient) GetLeague(ctx context.Context, leagueID string) (cocapi.League, error) {
+	return f.league, f.err
+}
+func (f *fakeCocapiClient) GetLeagueSeasons(ctx context.Context, leagueID string, query cocapi.QueryGetLeagueSeasons) (cocapi.LeagueSeasonListResponse, error) {
+	return f.leagueSeasons, f.err
+}
+func (f *fakeCocapiClient) GetLeagueSeasonRankings(ctx context.Context, leagueID, seasonID string, query cocapi.QueryGetLeagueSeasonRankings) (cocapi.PlayerRankingListResponse, error) {
+	return f.leagueRankings, f.err
+}
+func (f *fakeCocapiClient) GetLeagueTiers(ctx context.Context, query cocapi.QueryGetLeagueTiers) (cocapi.LeagueTierListResponse, error) {
+	return f.leagueTiers, f.err
+}
+func (f *fakeCocapiClient) GetLeagueTier(ctx context.Context, tierID string) (cocapi.LeagueTier, error) {
+	return f.leagueTier, f.err
+}
+func (f *fakeCocapiClient) GetLeagueHistory(ctx context.Context, playerTag string) (cocapi.LeagueSeasonResultListResponse, error) {
+	return f.leagueHistory, f.err
+}
+func (f *fakeCocapiClient) GetWarLeagues(ctx context.Context, query cocapi.QueryGetWarLeagues) (cocapi.WarLeagueListResponse, error) {
+	return f.warLeagues, f.err
+}
+func (f *fakeCocapiClient) GetWarLeague(ctx context.Context, leagueID string) (cocapi.WarLeague, error) {
+	return f.warLeague, f.err
+}
+func (f *fakeCocapiClient) GetClanLabels(ctx context.Context, query cocapi.QueryGetClanLabels) (cocapi.LabelListResponse, error) {
+	return f.clanLabels, f.err
+}
+func (f *fakeCocapiClient) GetPlayerLabels(ctx context.Context, query cocapi.QueryGetPlayerLabels) (cocapi.LabelListResponse, error) {
+	return f.playerLabels, f.err
+}
---
+++ internal/service/ranking_service.go
@@ -0,0 +1,174 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	cocapi "github.com/ww1489/WarSpark/pkg/cocapi"
+
+	"github.com/ww1489/WarSpark/internal/domain/ranking"
+	wardomain "github.com/ww1489/WarSpark/internal/domain/war"
+)
+
+type rankingCache interface {
+	GetLocations(ctx context.Context) (ranking.LocationListResponse, bool, error)
+	SetLocations(ctx context.Context, resp ranking.LocationListResponse, ttl time.Duration) error
+	GetClanRanking(ctx context.Context, locationID string) (ranking.ClanRankingListResponse, bool, error)
+	SetClanRanking(ctx context.Context, locationID string, resp ranking.ClanRankingListResponse, ttl time.Duration) error
+	GetPlayerRanking(ctx context.Context, locationID string) (ranking.PlayerRankingListResponse, bool, error)
+	SetPlayerRanking(ctx context.Context, locationID string, resp ranking.PlayerRankingListResponse, ttl time.Duration) error
+	GetClanCapitalRanking(ctx context.Context, locationID string) (ranking.ClanCapitalRankingListResponse, bool, error)
+	SetClanCapitalRanking(ctx context.Context, locationID string, resp ranking.ClanCapitalRankingListResponse, ttl time.Duration) error
+	GetClanBuilderBaseRanking(ctx context.Context, locationID string) (ranking.ClanBuilderBaseRankingListResponse, bool, error)
+	SetClanBuilderBaseRanking(ctx context.Context, locationID string, resp ranking.ClanBuilderBaseRankingListResponse, ttl time.Duration) error
+	GetPlayerBuilderBaseRanking(ctx context.Context, locationID string) (ranking.PlayerBuilderBaseRankingListResponse, bool, error)
+	SetPlayerBuilderBaseRanking(ctx context.Context, locationID string, resp ranking.PlayerBuilderBaseRankingListResponse, ttl time.Duration) error
+}
+
+type cocapiRankingClient interface {
+	GetLocations(ctx context.Context, query cocapi.QueryGetLocations) (cocapi.LocationListResponse, error)
+	GetClanRanking(ctx context.Context, locationID string, query cocapi.QueryGetClanRanking) (cocapi.ClanRankingListResponse, error)
+	GetPlayerRanking(ctx context.Context, locationID string, query cocapi.QueryGetPlayerRanking) (cocapi.PlayerRankingListResponse, error)
+	GetClanCapitalRanking(ctx context.Context, locationID string, query cocapi.QueryGetClanCapitalRanking) (cocapi.ClanCapitalRankingListResponse, error)
+	GetClanBuilderBaseRanking(ctx context.Context, locationID string, query cocapi.QueryGetClanBuilderBaseRanking) (cocapi.ClanBuilderBaseRankingListResponse, error)
+	GetPlayerBuilderBaseRanking(ctx context.Context, locationID string, query cocapi.QueryGetPlayerBuilderBaseRanking) (cocapi.PlayerBuilderBaseRankingListResponse, error)
+}
+
+type RankingService struct {
+	cocapi   cocapiRankingClient
+	cache    rankingCache
+	cacheTTL time.Duration
+}
+
+func NewRankingService(cocapi cocapiRankingClient, cache rankingCache, ttl time.Duration) *RankingService {
+	if ttl <= 0 { ttl = 10 * time.Minute }
+	return &RankingService{cocapi: cocapi, cache: cache, cacheTTL: ttl}
+}
+
+func (s *RankingService) GetLocations(ctx context.Context) (ranking.LocationListResponse, error) {
+	if s.cache != nil {
+		resp, ok, err := s.cache.GetLocations(ctx)
+		if err == nil && ok { return resp, nil }
+	}
+	cocapiResp, err := s.cocapi.GetLocations(ctx, cocapi.QueryGetLocations{})
+	if err != nil { return ranking.LocationListResponse{}, mapCocapiRankingError(err, "") }
+	resp := ranking.LocationListResponse{Paging: toDomainRankingPaging(cocapiResp.Paging)}
+	for _, loc := range cocapiResp.Items {
+		resp.Items = append(resp.Items, ranking.Location{
+			ID: loc.ID, Name: loc.Name, CountryCode: loc.CountryCode,
+			IsCountry: loc.IsCountry, LocalizedName: loc.LocalizedName,
+		})
+	}
+	if s.cache != nil { _ = s.cache.SetLocations(ctx, resp, s.cacheTTL) }
+	return resp, nil
+}
+
+func (s *RankingService) GetClanRanking(ctx context.Context, locationID string) (ranking.ClanRankingListResponse, error) {
+	if s.cache != nil {
+		resp, ok, err := s.cache.GetClanRanking(ctx, locationID)
+		if err == nil && ok { return resp, nil }
+	}
+	cocapiResp, err := s.cocapi.GetClanRanking(ctx, locationID, cocapi.QueryGetClanRanking{})
+	if err != nil { return ranking.ClanRankingListResponse{}, mapCocapiRankingError(err, wardomain.ErrorLocationNotFound) }
+	resp := ranking.ClanRankingListResponse{Paging: toDomainRankingPaging(cocapiResp.Paging)}
+	for _, entry := range cocapiResp.Items {
+		item := ranking.ClanRankingEntry{Tag: entry.Tag, Name: entry.Name, ClanLevel: entry.ClanLevel, ClanPoints: entry.ClanPoints, Members: entry.Members, Rank: entry.Rank, PreviousRank: entry.PreviousRank, BadgeURLs: entry.BadgeURLs}
+		if entry.Location.ID != 0 { item.Location = &ranking.Location{ID: entry.Location.ID, Name: entry.Location.Name, CountryCode: entry.Location.CountryCode, IsCountry: entry.Location.IsCountry, LocalizedName: entry.Location.LocalizedName} }
+		resp.Items = append(resp.Items, item)
+	}
+	if s.cache != nil { _ = s.cache.SetClanRanking(ctx, locationID, resp, s.cacheTTL) }
+	return resp, nil
+}
+
+func (s *RankingService) GetPlayerRanking(ctx context.Context, locationID string) (ranking.PlayerRankingListResponse, error) {
+	if s.cache != nil {
+		resp, ok, err := s.cache.GetPlayerRanking(ctx, locationID)
+		if err == nil && ok { return resp, nil }
+	}
+	cocapiResp, err := s.cocapi.GetPlayerRanking(ctx, locationID, cocapi.QueryGetPlayerRanking{})
+	if err != nil { return ranking.PlayerRankingListResponse{}, mapCocapiRankingError(err, wardomain.ErrorLocationNotFound) }
+	resp := ranking.PlayerRankingListResponse{Paging: toDomainRankingPaging(cocapiResp.Paging)}
+	for _, entry := range cocapiResp.Items {
+		item := ranking.PlayerRankingEntry{Tag: entry.Tag, Name: entry.Name, ExpLevel: entry.ExpLevel, Trophies: entry.Trophies, Rank: entry.Rank, PreviousRank: entry.PreviousRank, AttackWins: entry.AttackWins, DefenseWins: entry.DefenseWins}
+		if entry.Clan.Tag != "" { item.Clan = &ranking.ClanRef{Tag: entry.Clan.Tag, Name: entry.Clan.Name, BadgeURLs: entry.Clan.BadgeURLs} }
+		resp.Items = append(resp.Items, item)
+	}
+	if s.cache != nil { _ = s.cache.SetPlayerRanking(ctx, locationID, resp, s.cacheTTL) }
+	return resp, nil
+}
+
+func (s *RankingService) GetClanCapitalRanking(ctx context.Context, locationID string) (ranking.ClanCapitalRankingListResponse, error) {
+	if s.cache != nil {
+		resp, ok, err := s.cache.GetClanCapitalRanking(ctx, locationID)
+		if err == nil && ok { return resp, nil }
+	}
+	cocapiResp, err := s.cocapi.GetClanCapitalRanking(ctx, locationID, cocapi.QueryGetClanCapitalRanking{})
+	if err != nil { return ranking.ClanCapitalRankingListResponse{}, mapCocapiRankingError(err, wardomain.ErrorLocationNotFound) }
+	resp := ranking.ClanCapitalRankingListResponse{Paging: toDomainRankingPaging(cocapiResp.Paging)}
+	for _, entry := range cocapiResp.Items {
+		item := ranking.ClanCapitalRankingEntry{Tag: entry.Tag, Name: entry.Name, ClanLevel: entry.ClanLevel, ClanCapitalPoints: entry.ClanCapitalPoints, Rank: entry.Rank, PreviousRank: entry.PreviousRank, Members: entry.Members, BadgeURLs: entry.BadgeURLs}
+		if entry.Location.ID != 0 { item.Location = &ranking.Location{ID: entry.Location.ID, Name: entry.Location.Name, CountryCode: entry.Location.CountryCode, IsCountry: entry.Location.IsCountry, LocalizedName: entry.Location.LocalizedName} }
+		resp.Items = append(resp.Items, item)
+	}
+	if s.cache != nil { _ = s.cache.SetClanCapitalRanking(ctx, locationID, resp, s.cacheTTL) }
+	return resp, nil
+}
+
+func (s *RankingService) GetClanBuilderBaseRanking(ctx context.Context, locationID string) (ranking.ClanBuilderBaseRankingListResponse, error) {
+	if s.cache != nil {
+		resp, ok, err := s.cache.GetClanBuilderBaseRanking(ctx, locationID)
+		if err == nil && ok { return resp, nil }
+	}
+	cocapiResp, err := s.cocapi.GetClanBuilderBaseRanking(ctx, locationID, cocapi.QueryGetClanBuilderBaseRanking{})
+	if err != nil { return ranking.ClanBuilderBaseRankingListResponse{}, mapCocapiRankingError(err, wardomain.ErrorLocationNotFound) }
+	resp := ranking.ClanBuilderBaseRankingListResponse{Paging: toDomainRankingPaging(cocapiResp.Paging)}
+	for _, entry := range cocapiResp.Items {
+		item := ranking.ClanBuilderBaseRankingEntry{Tag: entry.Tag, Name: entry.Name, ClanLevel: entry.ClanLevel, ClanBuilderBasePoints: entry.ClanBuilderBasePoints, Rank: entry.Rank, PreviousRank: entry.PreviousRank, Members: entry.Members, BadgeURLs: entry.BadgeURLs}
+		if entry.Location.ID != 0 { item.Location = &ranking.Location{ID: entry.Location.ID, Name: entry.Location.Name, CountryCode: entry.Location.CountryCode, IsCountry: entry.Location.IsCountry, LocalizedName: entry.Location.LocalizedName} }
+		resp.Items = append(resp.Items, item)
+	}
+	if s.cache != nil { _ = s.cache.SetClanBuilderBaseRanking(ctx, locationID, resp, s.cacheTTL) }
+	return resp, nil
+}
+
+func (s *RankingService) GetPlayerBuilderBaseRanking(ctx context.Context, locationID string) (ranking.PlayerBuilderBaseRankingListResponse, error) {
+	if s.cache != nil {
+		resp, ok, err := s.cache.GetPlayerBuilderBaseRanking(ctx, locationID)
+		if err == nil && ok { return resp, nil }
+	}
+	cocapiResp, err := s.cocapi.GetPlayerBuilderBaseRanking(ctx, locationID, cocapi.QueryGetPlayerBuilderBaseRanking{})
+	if err != nil { return ranking.PlayerBuilderBaseRankingListResponse{}, mapCocapiRankingError(err, wardomain.ErrorLocationNotFound) }
+	resp := ranking.PlayerBuilderBaseRankingListResponse{Paging: toDomainRankingPaging(cocapiResp.Paging)}
+	for _, entry := range cocapiResp.Items {
+		item := ranking.PlayerBuilderBaseRankingEntry{Tag: entry.Tag, Name: entry.Name, ExpLevel: entry.ExpLevel, BuilderBaseTrophies: entry.BuilderBaseTrophies, Rank: entry.Rank, PreviousRank: entry.PreviousRank}
+		if entry.Clan.Tag != "" { item.Clan = &ranking.ClanRef{Tag: entry.Clan.Tag, Name: entry.Clan.Name, BadgeURLs: entry.Clan.BadgeURLs} }
+		resp.Items = append(resp.Items, item)
+	}
+	if s.cache != nil { _ = s.cache.SetPlayerBuilderBaseRanking(ctx, locationID, resp, s.cacheTTL) }
+	return resp, nil
+}
+
+func toDomainRankingPaging(p cocapi.Paging) ranking.Paging {
+	return ranking.Paging{Cursors: ranking.Cursors{After: p.Cursors.After, Before: p.Cursors.Before}}
+}
+
+func mapCocapiRankingError(err error, notFoundCode string) error {
+	switch {
+	case errors.Is(err, cocapi.ErrNotFound):
+		if notFoundCode != "" { return wardomain.NewError(notFoundCode, err.Error()) }
+		return wardomain.NewError(wardomain.ErrorWarNotFound, err.Error())
+	case errors.Is(err, cocapi.ErrAPINotConfigured):
+		return wardomain.NewError(wardomain.ErrorAPINotConfigured, err.Error())
+	case errors.Is(err, cocapi.ErrAPIAccessDenied):
+		return wardomain.NewError(wardomain.ErrorAPIAccessDenied, err.Error())
+	case errors.Is(err, cocapi.ErrAPIRequestFailed):
+		return wardomain.NewError(wardomain.ErrorAPIRequestFailed, err.Error())
+	case errors.Is(err, cocapi.ErrRateLimited):
+		return wardomain.NewError(wardomain.ErrorAPIRequestFailed, err.Error())
+	case errors.Is(err, cocapi.ErrAPIResponseInvalid):
+		return wardomain.NewError(wardomain.ErrorAPIResponseInvalid, err.Error())
+	default:
+		return err
+	}
+}
